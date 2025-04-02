75b8830510b445aaabcc9e1c88cedb44667bea7f
Jean-Claude Antonio <jcantonio@alineo.com>
2025-04-02 17:58:04 +0200

    fix warnings

--- lib/setupQuasarApp.ts
+++ lib/setupQuasarApp.ts
@@ -3,7 +3,7 @@ import { Quasar } from 'quasar'
 import quasarUserOptions from './quasar-user-options'
 
 // Import Quasar css
-import 'quasar/src/css/index.sass'
+import 'quasar/dist/quasar.css'
 
 export function setupQuasarApp(app: App) {
   app.use(Quasar, quasarUserOptions)
