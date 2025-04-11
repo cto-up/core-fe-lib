41d423f4f9011aef17a9c1c76f06f7f536a14820
Jean-Claude Antonio <jcantonio@alineo.com>
2025-04-11 10:57:06 +0200

    Auto close DateTime

--- lib/components/BDateTime.vue
+++ lib/components/BDateTime.vue
@@ -8,8 +8,17 @@
   >
     <template v-slot:prepend>
       <q-icon name="event" class="cursor-pointer">
-        <q-popup-proxy cover transition-show="scale" transition-hide="scale">
-          <q-date v-model="internalDate" mask="YYYY-MM-DD HH:mm">
+        <q-popup-proxy
+          v-model="showDatePopup"
+          cover
+          transition-show="scale"
+          transition-hide="scale"
+        >
+          <q-date
+            v-model="internalDate"
+            mask="YYYY-MM-DD HH:mm"
+            @update:model-value="onDateSelect($event)"
+          >
             <div class="row items-center justify-end">
               <q-btn v-close-popup label="Close" color="primary" flat></q-btn>
             </div>
@@ -20,9 +29,15 @@
 
     <template v-slot:append>
       <q-icon name="access_time" class="cursor-pointer">
-        <q-popup-proxy cover transition-show="scale" transition-hide="scale">
+        <q-popup-proxy
+          v-model="showTimePopup"
+          cover
+          transition-show="scale"
+          transition-hide="scale"
+        >
           <q-time
             v-model="internalDate"
+            @update:model-value="onTimeSelect($event)"
             mask="YYYY-MM-DD HH:mm"
             format24h
             :minute-options="[0, 15, 30, 45]"
@@ -38,14 +53,14 @@
 </template>
 
 <script lang="ts" setup>
-import { ref, watch } from "vue";
-import useDate from "../composables/useDate";
+import { ref, watch } from 'vue';
+import useDate from '../composables/useDate';
 const { ISOStringToDateTimeString, datetimeStringToISO } = useDate();
 
 const props = defineProps({
   modelValue: {
     type: String,
-    default: "",
+    default: '',
   },
   error: {
     type: Boolean,
@@ -58,16 +73,27 @@ const props = defineProps({
   },
   maxWidth: {
     type: String,
-    default: "300px",
+    default: '300px',
   },
 });
 
-const emit = defineEmits(["update:modelValue"]);
+const showDatePopup = ref<boolean>(false);
+const showTimePopup = ref<boolean>(false);
+
+const emit = defineEmits(['update:modelValue']);
 
 const internalDate = ref(ISOStringToDateTimeString(props.modelValue));
 
+const onDateSelect = (value: string) => {
+  showDatePopup.value = false;
+};
+
+const onTimeSelect = (value: string) => {
+  showTimePopup.value = false;
+};
+
 watch(internalDate, (newValue) => {
-  emit("update:modelValue", datetimeStringToISO(newValue));
+  emit('update:modelValue', datetimeStringToISO(newValue));
 });
 // Watch for changes in modelValue (prop) and update internalDate
 watch(
@@ -77,7 +103,7 @@ watch(
     if (newInternalDate !== internalDate.value) {
       internalDate.value = newInternalDate;
     }
-  }
+  },
 );
 </script>
 
