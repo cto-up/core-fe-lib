1b56751bcefd06a8a75030259f4aca6ccf17b33a
Jean-Claude Antonio <jcantonio@alineo.com>
2025-04-26 12:42:02 +0200

    rm greetings

--- lib/main.ts
+++ lib/main.ts
@@ -1,3 +1,2 @@
-export { default as Greeting } from "./components/Greeting.vue"
 export { i18n } from './i18n'
 export { setupQuasarApp } from './setupQuasarApp'
--- lib/utils/createGreetings.ts
+++
@@ -1,3 +0,0 @@
-export function createGreeting(name?: string): string {
-    return name === undefined ? "Hello!" : `Hello ${name}!`
-  }
