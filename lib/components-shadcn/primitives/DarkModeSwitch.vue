13cd1660b807b4d736cb4c1cf62673b380d117c6
Jean-Claude Antonio <jcantonio@alineo.com>
2026-05-18 21:46:21 +0200

    v2.3.0-shadcn-feedback

---
+++ lib/components-shadcn/primitives/DarkModeSwitch.vue
@@ -0,0 +1,16 @@
+<template>
+  <Button variant="ghost" size="icon" @click="toggle">
+    <Moon v-if="modelValue" class="h-5 w-5" />
+    <Sun v-else class="h-5 w-5" />
+  </Button>
+</template>
+
+<script lang="ts" setup>
+import { Button } from "../ui/button";
+import { Sun, Moon } from "lucide-vue-next";
+
+const props = defineProps<{ modelValue: boolean }>();
+const emit = defineEmits<{ "update:modelValue": [value: boolean] }>();
+
+const toggle = () => emit("update:modelValue", !props.modelValue);
+</script>
