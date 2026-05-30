a6883656a3d3ae5bea4d82bd189995b09f104aa5
Jean-Claude Antonio <jcantonio@alineo.com>
2026-05-30 13:35:50 +0200

    Improve Alert

--- lib/components-shadcn/ui/alert-dialog/AlertDialogAction.vue
+++ lib/components-shadcn/ui/alert-dialog/AlertDialogAction.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { type HTMLAttributes, computed } from "vue";
 import { AlertDialogAction, type AlertDialogActionProps } from "radix-vue";
+import { buttonVariants } from "../button";
 import { cn } from "../../utils";
 
 const props = defineProps<
@@ -17,12 +18,7 @@ const delegatedProps = computed(() => {
 <template>
   <AlertDialogAction
     v-bind="delegatedProps"
-    :class="
-      cn(
-        'inline-flex h-10 items-center justify-center rounded-md bg-primary px-4 py-2 text-sm font-semibold text-primary-foreground ring-offset-background transition-colors hover:bg-primary/90 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50',
-        props.class
-      )
-    "
+    :class="cn(buttonVariants(), props.class)"
   >
     <slot />
   </AlertDialogAction>
